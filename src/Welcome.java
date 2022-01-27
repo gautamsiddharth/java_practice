4cfdcdf262ad75315a8b62f91a937dd8f4ebfacc
siddharth <gautamsiddarth8@gmail.com>
2022-01-27 14:29:13 -0600

    Initial Commit

---
+++ src/Welcome.java
@@ -0,0 +1,12 @@
+public class Welcome {
+
+    public static void main(String[] args){
+        String Greeting = "Welcome tp Core Java !";
+        System.out.println(Greeting);
+        for(int i=0; i < Greeting.length(); i++){
+            System.out.print("=");
+        }
+        System.out.println();
+
+    }
+}

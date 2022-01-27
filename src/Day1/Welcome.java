687ac17409310aa0c1ec7ca9855d33f9aa2f1174
siddharth <gautamsiddarth8@gmail.com>
2022-01-27 14:34:33 -0600

    Changed some minor mistake

--- src/Day1/Welcome.java
+++ src/Day1/Welcome.java
@@ -1,7 +1,9 @@
+package Day1;
+
 public class Welcome {
 
     public static void main(String[] args){
-        String Greeting = "Welcome tp Core Java !";
+        String Greeting = "Day1.Welcome to Core Java !";
         System.out.println(Greeting);
         for(int i=0; i < Greeting.length(); i++){
             System.out.print("=");

f925e023b48f89d5643361f4c469127f74a30326
siddharth <gautamsiddarth8@gmail.com>
2022-01-27 15:50:07 -0600

    Data types

---
+++ src/day2/student.java
@@ -0,0 +1,36 @@
+package day2;
+
+public class student {
+
+    // name
+    // phoneNumber
+    // dob
+    // class
+    // rollNumber
+    // address
+    // idNumber
+    // email
+    // username
+    // password
+    // emergencyContactNumber
+    // socialSecurityNumber
+
+
+    String name;
+    String phoneNumber;
+    String dob;
+    String sClass;
+    String address;
+    String rollNumber;
+    int idNUmber;
+    String email;
+    String username;
+    String password;
+    String emergencyContactNumber;
+    String SocialSecurityNumber;
+
+
+
+
+
+}
